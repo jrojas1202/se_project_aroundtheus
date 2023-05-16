438b8c88eb57ae84eb031511df8165497cd3a9de
John Rojas <jrojas1202@gmail.com>
2023-05-16 13:00:53 -0400

    Sprint 4 part 2 update

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,45 @@
+// WRAPPERS
+const profileEditModal = document.querySelector("#profile-edit-modal");
+const profileCloseModal = document.querySelector("#profile-close-modal");
+const profileEditForm = profileEditModal.querySelector(".modal__form");
+const modalContainer = document.querySelector(".modal__container");
+
+// BUTTONS AND OTHER DOM NODES
+const profileEditButton = document.querySelector("#profile-edit-button");
+const profileModalCloseButton = profileEditModal.querySelector(".modal__close");
+
+const imageProfileModal = document.querySelector("#image-modal");
+
+// FORM INPUT DATA
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+const profileTitleInput = document.querySelector("#profile-title-input");
+const profileDescriptionInput = document.querySelector(
+  "#profile-description-input"
+);
+
+// FUNCTIONS
+
+function handleEscape(event) {
+  if (event.key === "Escape") {
+    const openedModal = document.querySelector(".modal_opened");
+    closeModal(openedModal);
+  }
+}
+
+function openModal(modal) {
+  modal.classList.add("modal_opened");
+}
+
+// Define for OpenModal Function
+const editButton = document.querySelector(".profile__edit-button");
+
+// Definitions for saveAndClose Function
+const saveButton = document.querySelector(".modal__button");
+
+// Get the cards list element
+const cardsList = document.querySelector(".cards__list");
+
 const initialCards = [
   {
     name: "Yosemite Valley",
@@ -24,3 +66,39 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   },
 ];
+
+// Call the renderInitialCards function to populate the initial cards on page load
+
+function openEditProfileModal() {
+  openModal(profileEditModal);
+}
+
+function closeModal(modal) {
+  modal.classList.remove("modal_opened");
+}
+
+function handProfileFormSubmit(event) {
+  event.preventDefault();
+
+  // New Values entered
+  const newTitle = titleInput.value;
+  const newDescription = descriptionInput.value;
+
+  // Update the values
+  profileTitle.textContent = newTitle;
+  profileDescription.textContent = newDescription;
+
+  closeModal(profileEditModal);
+}
+
+// Event Listeners
+
+profileEditButton.addEventListener("click", () => {
+  profileTitleInput.value = profileTitle.textContent;
+  profileDescriptionInput.value = profileDescription.textContent;
+  openModal(profileEditModal);
+});
+
+profileModalCloseButton.addEventListener("click", () =>
+  closeModal(profileEditModal)
+);
