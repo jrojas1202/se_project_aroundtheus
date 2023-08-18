0d1bde83d48a5c5a633a9d44ddfdd1e53d2b4b75
John Rojas <jrojas1202@gmail.com>
2023-08-18 15:11:38 -0400

    camelcase fix mk3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import Section from "../components/Section.js";
 import Card from "../components/Card.js";
-import popupWithImage from "../components/popupWitImage.js";
-import PopupWithForm from "../components/popupWitForm.js";
+import popupWithImage from "../components/PopupWithImage.js";
+import PopupWithForm from "../components/PopupWithForm.js";
 import UserInfo from "../components/UserInfo.js";
 import FormValidator from "../components/FormValidator.js";
 import "./index.css";
